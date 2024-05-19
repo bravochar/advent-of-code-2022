0817d2ddf69572d857b525b7a4aa8206a10bfbb7
bravochar <4663128+bravochar@users.noreply.github.com>
2024-05-19 08:23:16 -0400

    Optimized Day 16 Part 1
    Added better pruning heuristics to more accurately calculate the ideal
    remaining score and to visit all closed valves instead of _all_ valves
    when expanding paths

--- day-16/src/main.rs
+++ day-16/src/main.rs
@@ -5,8 +5,8 @@ use std::fs::File;
 use std::io::{prelude::*, BufReader};
 use std::collections::HashMap;
 
-const FILENAME: &str = "./input";
-//const FILENAME: &str = "./test";
+//const FILENAME: &str = "./input";
+const FILENAME: &str = "./test";
 const NUM_MIN: i32 = 30;
 
 #[derive(Clone, Debug)]
@@ -120,28 +120,37 @@ impl <'v> fmt::Display for Step<'v> {
 struct Path<'v> {
     steps: Vec<Step<'v>>,
     open_valves: Vec<&'v Valve>,
+    closed_valves: Vec<&'v Valve>,
     current_flow: i32,
     total_flow: i32,
+    cur_valve: &'v Valve,
+    rem_time: i32,
 }
 
 impl <'v> Path<'v> {
-    fn new() -> Path<'v> {
-        Path {
+    fn new(closed_valves: Vec<&'v Valve>, v: &'v Valve, rem_time: i32) -> Path<'v> {
+        let mut p = Path {
             steps: Vec::new(),
             open_valves: Vec::new(),
+            closed_valves,
             current_flow: 0,
             total_flow: 0,
-        }
+            cur_valve: v,
+            rem_time: rem_time + 1
+        };
+
+        p.add_step(Step::Move(v));
+
+        p
     }
 
+    #[allow(dead_code)]
     fn to_string(&self) -> String {
         let mut rval = String::new();
 
         let mut open_valves: Vec<String> = Vec::new();
         let mut current_flow = 0;
-        let mut total_flow = 0;
         for (i, s) in self.steps.iter().enumerate() {
-            total_flow += current_flow;
             rval += &format!("== Minute {} ==\n", i);
             match open_valves.len() {
                 0 => rval.push_str("No valves are open\n"),
@@ -169,22 +178,42 @@ impl <'v> Path<'v> {
         rval + &format!("Total_flow: {}", self.final_score())
     }
 
+    fn open_valve(&mut self, v: &'v Valve) {
+        self.closed_valves.retain(|x| {
+            !&x.name.contains(&v.name)});
+        self.open_valves.push(v);
+        self.current_flow += v.flow_rate;
+    }
+
     fn add_step(&mut self, s: Step<'v>) {
         self.total_flow += self.current_flow;
         match s {
             Step::Open(v) => {
-                self.open_valves.push(v);
-                self.current_flow += v.flow_rate;
+                self.open_valve(v);
+                self.cur_valve = v
             },
-            _ => ()
+            Step::Move(v) => self.cur_valve = v
         }
         self.steps.push(s);
+        self.rem_time -= 1;
     }
 
     fn final_score(&self) -> i32 {
-        let rem_time = NUM_MIN + 1 - self.steps.len() as i32;
+        self.total_flow + self.current_flow * self.rem_time
+    }
 
-        self.total_flow + self.current_flow * rem_time
+    fn ideal_score(&self) -> i32 {
+        let mut rval = self.total_flow;
+        let mut flow = self.current_flow;
+        
+        for i in 0..self.rem_time {
+            rval += flow;
+            match self.closed_valves.get(i as usize) {
+                Some(v) => flow += v.flow_rate,
+                _ => ()
+            }
+        }
+        rval
     }
 
     fn next_steps(&self) -> Vec<String> {
@@ -193,14 +222,6 @@ impl <'v> Path<'v> {
 
         let v = match last_step {
             Step::Move(v) => {
-                /*
-                if !self.open_valves.iter().any(
-                    |x| {x.eq(v)}) {
-                    if v.flow_rate > 0 {
-                        rval.push(v.name.clone());
-                    }
-                }
-                 */
                 v
             }, 
             Step::Open(v) => v
@@ -213,23 +234,13 @@ impl <'v> Path<'v> {
         rval
     }
 
+    #[allow(dead_code)]
     fn path_len(&self) -> i32 {
         return self.steps.len() as i32 - 1
     }
 
-    fn cur_valve(&self) -> &'v Valve {
-        match self.steps.last().unwrap() {
-            Step::Move(v) => v,
-            Step::Open(v) => v
-        }
-    }
-
-    fn cur_pos(&self) -> &str {
-        &self.cur_valve().name
-    }
-
     fn can_open(&self) -> bool {
-        let v = self.cur_valve();
+        let v = self.cur_valve;
 
         if v.flow_rate > 0 && !self.open_valves.iter().any(|x| {
             x.name.contains(&v.name)}) {
@@ -255,14 +266,17 @@ fn open_next_valve<'v>(
     let mut visited_valves: Vec<String> = Vec::new();
     poss_paths.push(p.clone());
 
-    let mut i = 0;
     while !poss_paths.is_empty() {
         let mut new_steps: Vec<Path> = Vec::new();
         for p in poss_paths.into_iter() {
             if p.can_open() {
                 let mut new_path = p.clone();
-                new_path.add_step(Step::Open(p.cur_valve()));
+                new_path.add_step(Step::Open(p.cur_valve));
                 rvals.push(new_path);
+                
+            } else if p.closed_valves.len() == 0 {
+                //println!("No more valves to open");
+                continue
             }
 
             for v_name in p.next_steps().iter() {
@@ -283,7 +297,6 @@ fn open_next_valve<'v>(
         }
 
         poss_paths = new_steps;
-        i += 1;
         /*
         println!("{} paths after {} iterations", poss_paths.len(), i);
         for p in poss_paths.iter() {
@@ -300,31 +313,26 @@ fn part_1() {
 
     // hashmap to find next move
     let mut valve_map = HashMap::new();
+    let mut closed_valves = Vec::new();
     for v in valves.iter() {
         valve_map.insert(v.name.clone(), v);
-        println!("{}", v);
+        if v.flow_rate > 0 {
+            closed_valves.push(v);
+        }
+        //println!("{}", v);
     }
+
+    println!("Need to open {} valves...", closed_valves.len());
+    closed_valves.sort_by(|a, b| {
+        b.flow_rate.cmp(&a.flow_rate)
+    });
+    for v in closed_valves.iter() {
+        println!("  {}: {}", v.name, v.flow_rate);
+    }
+    closed_valves.sort_by(|a,b| {b.flow_rate.cmp(&a.flow_rate)});
     let first_step = valve_map.get("AA").unwrap();
 
-    /*
-     * With 30 decisions to make, most with at least two options,
-     * we cannot do a breadth-first search, and depth first would
-     * involve a _lot_ of calculations. We'll need to implement a
-     * greedy search to look at the best options first?
-     * 
-     * We can start with a simple 1-optimal step, but we'll likely
-     * need to up that to 2- or 3-optimal before we're really done.
-     * 
-     * WRONG: We need to be able to move toward the valve with the largest flow
-     * rate in order to get it open ASAP. 
-     * 
-     * TODO: calculate the opportunity cost of runing on a "large" valve along
-     * that path - will it generate enough output to justify delaying opening
-     * the larger valve by one minute. Actually, that's pretty simple!
-     */
-    let mut p = Path::new();
-    p.add_step(
-        Step::Move(first_step));
+    let p = Path::new(closed_valves, &first_step, NUM_MIN);
 
     let mut valves_left: Vec<&Valve> = valves.iter().filter(
         |v| {v.flow_rate > 0}).collect();
@@ -333,11 +341,13 @@ fn part_1() {
     let mut high_score = 0;
     let mut best_path = p.clone();
     let mut paths = vec![p];
-    //while !paths.is_empty() {
     while !paths.is_empty() {
         let mut next_paths: Vec<Path> = Vec::new();
         for path in paths.into_iter() {
-            if path.final_score() > high_score {
+            if path.ideal_score() <= high_score {
+                continue;
+
+            } else if path.final_score() > high_score {
                 high_score = path.final_score();
                 best_path = path.clone();
             }
@@ -350,12 +360,18 @@ fn part_1() {
         }
 
         paths = next_paths;
-        println!("Working with {} paths", paths.len());
+        println!("Working with {} paths; best {}", paths.len(), high_score);
     }
     println!("Best path of {} steps: {}",
         best_path.steps.len(),
         high_score);
-    println!("{}", best_path.to_string());
+    
+    if FILENAME.eq("./test") {
+        assert_eq!(high_score, 1651);
+    } else {
+        assert_eq!(high_score, 1673);
+    }
+    //println!("{}", best_path.to_string());
 }
 
 fn part_2() {
