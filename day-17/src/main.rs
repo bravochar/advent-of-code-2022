789a586d6db6a9e538a98202c806c08d5935d51b
bravochar <4663128+bravochar@users.noreply.github.com>
2024-05-25 16:03:26 -0400

    Day 17 - 1 shape implemented and dropping
    Changed from Point to just rows of x coords - new need to record the Y
    coordinat twice, both as the key of the hashmap and in the Point

--- day-17/src/main.rs
+++ day-17/src/main.rs
@@ -2,6 +2,7 @@ const FILENAME: &str = "./input";
 //const FILENAME: &str = "./test";
 
 use core::cmp::Ordering;
+use std::hash::Hash;
 use std::{fmt, vec};
 use std::cmp::max;
 use std::fs::read_to_string;
@@ -59,29 +60,197 @@ mod tests {
     }
 }
 
-
-#[derive(PartialEq, Eq, PartialOrd, Ord, Clone)]
-struct Point {
-    x: i32,
-    y: i32,
+#[derive(Debug)]
+struct Shape {
+    rows: HashMap<i32, Vec<i32>>,
 }
 
-impl Point {
-    fn new(x: i32, y:i32) -> Point {
-        Point {x, y}
+impl Shape {
+    /*
+     * Each rock appears so that its left edge is two units away from the left
+     * wall and its bottom edge is three units above the highest rock in the 
+     * room (or the floor, if there isn't one).
+     */
+    fn horizontal_line(high_point: i32) -> Shape {
+        /*
+         * ####
+         */
+        let base_y = high_point + 3;
+        let mut rows = HashMap::new();
+        let mut row = Vec::new();
+        for x in 2..6 {
+            row.push(x);
+        }
+        rows.insert(base_y, row);
+
+        Shape{rows}
+    }
+
+    fn high_point(&self) -> i32 {
+        self.rows.keys().max().unwrap().to_owned()
     }
 }
 
 struct Shaft {
-    rows: HashMap<i32, Vec<Point>>,
-    high_point: i32
+    jets: JetStream,
+    rows: HashMap<i32, Vec<i32>>,
+    high_point: i32,
+    shape: Option<Shape>
 }
 
 const SHAFT_WIDTH: i32 = 7;
 impl Shaft {
-    fn new() -> Self {
+    fn new(jets: JetStream) -> Self {
         let rows = HashMap::new();
-        Shaft { rows, high_point: 0 }
+        Shaft {
+            jets,
+            rows,
+            high_point: 0,
+            shape: None }
+    }
+
+    fn add_shape(&mut self, s: Shape) {
+        match self.shape {
+            None => self.shape = Some(s),
+            _ => panic!("Cannot add another shape")
+        }
+    }
+
+    fn move_right(&mut self) {
+        let s = self.shape.as_mut().unwrap();
+
+        /*
+         * ensure that all points can move right without:
+         *  1) moving beyond the wall at SHAFT_WIDTH
+         *  2) colliding with existing rock
+         */
+        let mut new_rows = HashMap::new();
+        for (y, shape_points) in s.rows.iter() {
+            let mut new_row = Vec::new();
+            for x in shape_points.iter() {
+                let new_x = x + 1;
+                if new_x >= SHAFT_WIDTH {
+                    println!("Collision with wall - not moving");
+                    return;
+                }
+                match self.rows.get(y) {
+                    Some(rock_points) => {
+                        if rock_points.contains(&new_x) {
+                            println!("Collision with rock - not moving");
+                            return;
+                        }
+                    },
+                    _ => ()
+                }
+                new_row.push(new_x);
+            };
+            new_rows.insert(y.to_owned(), new_row);
+        }
+        s.rows = new_rows;
+    }
+
+    fn move_left(&mut self) {
+        let s = self.shape.as_mut().unwrap();
+
+        /*
+         * ensure that all points can move right without:
+         *  1) moving beyond the wall at SHAFT_WIDTH
+         *  2) colliding with existing rock
+         */
+        let mut new_rows = HashMap::new();
+        for (y, shape_points) in s.rows.iter() {
+            let mut new_row = Vec::new();
+            for x in shape_points.iter() {
+                let new_x = x - 1;
+                if new_x < 0 {
+                    println!("Collision with wall - not moving");
+                    return;
+                }
+                match self.rows.get(y) {
+                    Some(rock_points) => {
+                        if rock_points.contains(&new_x) {
+                            println!("Collision with rock - not moving");
+                            return;
+                        }
+                    },
+                    _ => ()
+                }
+                new_row.push(new_x);
+            };
+            new_rows.insert(y.to_owned(), new_row);
+        }
+        s.rows = new_rows;
+    }
+
+    fn jet_turn(&mut self) {
+        let jet_char = self.jets.next().unwrap();
+
+        if jet_char == '>' {
+            self.move_right();
+        } else {
+            self.move_left();
+        }
+    }
+
+    fn petrify_shape(&mut self) {
+        let s = self.shape.as_mut().unwrap();
+        for (y, shape_points) in s.rows.iter() {
+            let mut row = match self.rows.remove(y) {
+                Some(r) => r,
+                None => Vec::new()
+            };
+            for x in shape_points.into_iter() {
+                row.push(x.clone());
+            }
+            self.rows.insert(y.clone(), row);
+        }
+
+        self.shape = None;
+    }
+
+    fn gravity_turn(&mut self) -> bool {
+        let s = self.shape.as_mut().unwrap();
+
+        /*
+         * ensure that all points can move right without:
+         *  1) moving beyond the wall at SHAFT_WIDTH
+         *  2) colliding with existing rock
+         */
+        let mut new_rows = HashMap::new();
+        for (y, shape_points) in s.rows.iter() {
+            let mut new_row = Vec::new();
+            let new_y = y - 1;
+            if new_y < 0 {
+                self.petrify_shape();
+                return true;
+            }
+            for x in shape_points.iter() {
+                match self.rows.get(&new_y) {
+                    Some(rock_points) => {
+                        if rock_points.contains(&x) {
+                            println!("Collision with rock - not falling");
+                            self.petrify_shape();
+                            return true;
+                        }
+                    },
+                    _ => ()
+                }
+                new_row.push(x.to_owned());
+            };
+            new_rows.insert(new_y.to_owned(), new_row);
+        }
+        s.rows = new_rows;
+        
+        return false
+    }
+
+    fn full_turn(&mut self) -> bool {
+        self.jet_turn();
+        self.gravity_turn()
+    }
+
+    fn drop_shape(&mut self) {
+        while !self.full_turn() {()}
     }
 }
 
@@ -90,34 +259,44 @@ impl fmt::Display for Shaft {
         let mut rval = fmt::Result::Ok(());
 
         // TODO: account for when shape make highest point more than this
-        let mut y = self.high_point + 3;
+        let mut y = match self.shape {
+            None => self.high_point + 3,
+            Some(_) => self.shape.as_ref().unwrap().high_point()
+        };
+
         while y >= 0 && rval.is_ok() {
             // TODO: make this a Vec<&Point> and add points from both
             //      the shaft and the falling piece
             //
-            let mut row_points = match self.rows.get(&y){
-                None => {
-                    rval = write!(f, "|.......|\n");
-                    y -= 1;
-                    continue
+            let rock_points = self.rows.get(&y);
+            let shape_points = match self.shape.as_ref() {
+                Some(s) => {
+                    s.rows.get(&y)
                 },
-                Some(r) => r.clone()
+                None => None
             };
-            row_points.sort();
 
             let mut row_str = "|".to_owned();
-            let mut x = 0;
-            for p in row_points.iter() {
-                while x < p.x {
-                    row_str.push('.');
-                    x += 1;
+            for x in 0..SHAFT_WIDTH {
+                match shape_points {
+                    Some(v) => {
+                        if v.contains(&x) {
+                            row_str.push('@');
+                            continue;
+                        }
+                    },
+                    _ => ()
                 }
-                row_str.push('#');
-                x += 1;
-            }
-            while x < SHAFT_WIDTH {
+                match rock_points {
+                    Some(v) => {
+                        if v.contains(&x) {
+                            row_str.push('#');
+                            continue;
+                        }
+                    },
+                    _ => ()
+                }
                 row_str.push('.');
-                x += 1;
             }
             row_str.push('|');
             rval = write!(f, "{}\n", row_str);
@@ -131,8 +310,22 @@ impl fmt::Display for Shaft {
 }
 
 fn part_1(jets: JetStream) -> i32 {
-    let mut shaft = Shaft::new();
+    let mut shaft = Shaft::new(jets);
 
+    shaft.add_shape(Shape::horizontal_line(shaft.high_point));
+    println!("{}", shaft);
+    shaft.jet_turn();
+    println!("{}", shaft);
+    shaft.gravity_turn();
+    println!("{}", shaft);
+    
+    shaft.full_turn();
+    println!("{}", shaft);
+    
+    shaft.full_turn();
+    println!("{}", shaft);
+    
+    shaft.full_turn();
     println!("{}", shaft);
 
     shaft.high_point
