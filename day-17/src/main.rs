40cd709b793793c8099b82e497bfeb09384e692c
bravochar <4663128+bravochar@users.noreply.github.com>
2024-05-27 07:36:22 -0400

    Added tests for first 10 dorps
    Checking this in before refactoring shape. I want to move to only tracking the bottom-left corner of the shape to avoid all of the copies. all drops and moves will then only have to update either the X or Y value in Shape to speed things up.

--- day-17/src/main.rs
+++ day-17/src/main.rs
@@ -1,28 +1,29 @@
-const FILENAME: &str = "./input";
-//const FILENAME: &str = "./test";
+//const FILENAME: &str = "./input";
+const FILENAME: &str = "./test";
 
-use core::cmp::Ordering;
-use std::hash::Hash;
 use std::{fmt, vec};
-use std::cmp::max;
 use std::fs::read_to_string;
-use std::io::{prelude::*, BufReader};
-use std::collections::BinaryHeap;
 use std::collections::HashMap;
 
+#[derive(Clone)]
 struct JetStream {
     filename: String,
-    chars: Vec<char>
+    chars: Vec<char>,
+    cycle_len: i32,
+    chars_left: i32,
 }
 
 impl JetStream {
     fn new(filename: &str) -> JetStream {
         let buf = read_to_string(&filename).unwrap();
-        let chars = buf.chars().rev().collect();
+        let chars: Vec<char> = buf.chars().rev().collect();
+        let cycle_len = chars.len() as i32;
 
         JetStream{
             filename: filename.to_owned(),
-            chars}
+            chars,
+            cycle_len,
+            chars_left: cycle_len}
     }
 }
 
@@ -31,10 +32,13 @@ impl <'v> Iterator for JetStream {
 
     fn next(&mut self) -> Option<char> {
         match self.chars.pop() {
-            Some(c) => Some(c),
+            Some(c) => {
+                self.chars_left -= 1;
+                Some(c)},
             _ => {
                 let buf = read_to_string(&self.filename).unwrap();
                 self.chars = buf.chars().rev().collect();
+                self.chars_left = self.cycle_len - 1;
                 Some(self.chars.pop().unwrap())
             }
         }
@@ -45,6 +49,11 @@ impl <'v> Iterator for JetStream {
 mod tests {
     // Note this useful idiom: importing names from outer (for mod tests) scope.
     use super::*;
+
+    fn get_test_shaft() -> Shaft {
+        let jets = JetStream::new("./test");
+        Shaft::new(jets)
+    }
 
     #[test]
     fn test_jet_stream() {
@@ -58,11 +67,133 @@ mod tests {
             assert_eq!(c, js.next().unwrap());
         }
     }
+    
+    #[test]
+    fn test_first_shape() {
+        let mut shaft = get_test_shaft();
+
+        shaft.add_next_shape();
+        shaft.drop_shape();
+        let shaft_str = format!("{}", shaft);
+        let test_str = "|..####.|\n\
+                              +-------+\n";
+        println!("{}", shaft_str);
+        println!("{}", test_str);
+        assert_eq!(shaft_str, test_str);
+    }
+    
+    #[test]
+    fn test_second_shape() {
+        let mut shaft = get_test_shaft();
+
+        for _ in 0..2 {
+            shaft.add_next_shape();
+            shaft.drop_shape();
+        }
+        let shaft_str = format!("{}", shaft);
+        let test_str = "\
+        |...#...|\n\
+        |..###..|\n\
+        |...#...|\n\
+        |..####.|\n\
+        +-------+\n";
+        println!("{}", shaft_str);
+        println!("{}", test_str);
+        assert_eq!(shaft_str, test_str);
+    }
+    
+    #[test]
+    fn test_third_shape() {
+        let mut shaft = get_test_shaft();
+
+        for _ in 0..3 {
+            shaft.add_next_shape();
+            shaft.drop_shape();
+        }
+        let shaft_str = format!("{}", shaft);
+        let test_str = "\
+        |..#....|\n\
+        |..#....|\n\
+        |####...|\n\
+        |..###..|\n\
+        |...#...|\n\
+        |..####.|\n\
+        +-------+\n";
+        println!("{}", shaft_str);
+        println!("{}", test_str);
+        assert_eq!(shaft_str, test_str);
+    }
+
+    #[test]
+    fn test_09_shapes() {
+        let mut shaft = get_test_shaft();
+
+        for _ in 0..9 {
+            shaft.add_next_shape();
+            shaft.drop_shape();
+        }
+        let shaft_str = format!("{}", shaft);
+        let test_str = "\
+        |....#..|\n\
+        |....#..|\n\
+        |....##.|\n\
+        |....##.|\n\
+        |..####.|\n\
+        |.###...|\n\
+        |..#....|\n\
+        |.####..|\n\
+        |....##.|\n\
+        |....##.|\n\
+        |....#..|\n\
+        |..#.#..|\n\
+        |..#.#..|\n\
+        |#####..|\n\
+        |..###..|\n\
+        |...#...|\n\
+        |..####.|\n\
+        +-------+\n";
+        println!("{}", shaft_str);
+        println!("{}", test_str);
+        assert_eq!(shaft_str, test_str);
+    }
+
+    #[test]
+    fn test_10_shapes() {
+        let mut shaft = get_test_shaft();
+
+        for _ in 0..10 {
+            shaft.add_next_shape();
+            shaft.drop_shape();
+        }
+        let shaft_str = format!("{}", shaft);
+        let test_str = "\
+        |....#..|\n\
+        |....#..|\n\
+        |....##.|\n\
+        |##..##.|\n\
+        |######.|\n\
+        |.###...|\n\
+        |..#....|\n\
+        |.####..|\n\
+        |....##.|\n\
+        |....##.|\n\
+        |....#..|\n\
+        |..#.#..|\n\
+        |..#.#..|\n\
+        |#####..|\n\
+        |..###..|\n\
+        |...#...|\n\
+        |..####.|\n\
+        +-------+\n";
+        println!("{}", shaft_str);
+        println!("{}", test_str);
+        assert_eq!(shaft_str, test_str);
+    }
 }
 
 #[derive(Debug)]
 struct Shape {
-    rows: HashMap<i32, Vec<i32>>,
+    rows: HashMap<i64, Vec<i64>>,
 }
 
 impl Shape {
@@ -71,48 +202,131 @@ impl Shape {
      * wall and its bottom edge is three units above the highest rock in the 
      * room (or the floor, if there isn't one).
      */
-    fn horizontal_line(high_point: i32) -> Shape {
+    fn horizontal_line(high_point: i64) -> Shape {
         /*
          * ####
          */
-        let base_y = high_point + 3;
+        let base_y = high_point + 4;
         let mut rows = HashMap::new();
-        let mut row = Vec::new();
-        for x in 2..6 {
-            row.push(x);
-        }
-        rows.insert(base_y, row);
+        rows.insert(base_y, vec![2, 3, 4, 5]);
 
         Shape{rows}
     }
 
-    fn high_point(&self) -> i32 {
+    fn cross(high_point: i64) -> Shape {
+        /*
+         * .#.
+         * ###
+         * .#.
+         */
+        let base_y = high_point + 4;
+        let mut rows = HashMap::new();
+        rows.insert(base_y, vec![3]);
+        rows.insert(base_y + 1, vec![2, 3, 4]);
+        rows.insert(base_y + 2, vec![3]);
+
+        Shape{rows}
+    }
+
+    fn ell(high_point: i64) -> Shape {
+        /*
+         * ..#
+         * ..#
+         * ###
+         */
+        let base_y = high_point + 4;
+        let mut rows = HashMap::new();
+        rows.insert(base_y, vec![2, 3, 4]);
+        rows.insert(base_y + 1, vec![4]);
+        rows.insert(base_y + 2, vec![4]);
+
+        Shape{rows}
+    }
+
+    fn vertical_line(high_point: i64) -> Shape {
+        /*
+         * ####
+         */
+        let base_y = high_point + 4;
+        let mut rows = HashMap::new();
+        rows.insert(base_y, vec![2]);
+        rows.insert(base_y + 1, vec![2]);
+        rows.insert(base_y + 2, vec![2]);
+        rows.insert(base_y + 3, vec![2]);
+
+        Shape{rows}
+    }
+
+    fn square(high_point: i64) -> Shape {
+        /*
+         * ####
+         */
+        let base_y = high_point + 4;
+        let mut rows = HashMap::new();
+        rows.insert(base_y, vec![2, 3]);
+        rows.insert(base_y + 1, vec![2, 3]);
+
+        Shape{rows}
+    }
+
+    fn high_point(&self) -> i64 {
         self.rows.keys().max().unwrap().to_owned()
     }
 }
 
 struct Shaft {
     jets: JetStream,
-    rows: HashMap<i32, Vec<i32>>,
-    high_point: i32,
-    shape: Option<Shape>
+    rows: HashMap<i64, Vec<i64>>,
+    high_point: i64,
+    shape: Option<Shape>,
+    next_shape: i64,
+    shape_count: i64,
+    left_closure: i64,
+    right_closure: i64,
 }
 
-const SHAFT_WIDTH: i32 = 7;
+const SHAFT_WIDTH: i64 = 7;
 impl Shaft {
     fn new(jets: JetStream) -> Self {
         let rows = HashMap::new();
         Shaft {
             jets,
             rows,
-            high_point: 0,
-            shape: None }
+            high_point: -1,
+            shape: None,
+            next_shape: 0,
+            shape_count: 0,
+            left_closure: 0,
+            right_closure: 0,
+        }
+
     }
 
     fn add_shape(&mut self, s: Shape) {
         match self.shape {
             None => self.shape = Some(s),
             _ => panic!("Cannot add another shape")
+        }
+    }
+
+    fn add_next_shape(&mut self) {
+        match self.next_shape {
+            0 => {
+                if self.jets.chars_left == 0 {
+                    println!("Jetstream looped after {} shapes", self.shape_count);
+                }
+                self.add_shape(Shape::horizontal_line(self.high_point))
+            },
+            1 => self.add_shape(Shape::cross(self.high_point)),
+            2 => self.add_shape(Shape::ell(self.high_point)),
+            3 => self.add_shape(Shape::vertical_line(self.high_point)),
+            _ => self.add_shape(Shape::square(self.high_point))
+        }
+        self.next_shape = (self.next_shape + 1) % 5;
+        self.shape_count += 1;
+
+        if self.shape_count % 8096 == 0 {
+            self.prune_dead_rows();
         }
     }
 
@@ -123,27 +337,47 @@ impl Shaft {
          * ensure that all points can move right without:
          *  1) moving beyond the wall at SHAFT_WIDTH
          *  2) colliding with existing rock
+         * 
+         *  TODO: we only need to check for free space to the RIGHT of the 
+         *      right-most point in each row, and then "move" the left-most
+         *      point into that free space
          */
         let mut new_rows = HashMap::new();
         for (y, shape_points) in s.rows.iter() {
-            let mut new_row = Vec::new();
-            for x in shape_points.iter() {
-                let new_x = x + 1;
+            // get min and max value
+            let mut new_row = shape_points.clone();
+            if true {
+                let min_x = shape_points.iter().min().unwrap();
+                let max_x = shape_points.iter().max().unwrap();
+                let new_x = max_x + 1;
                 if new_x >= SHAFT_WIDTH {
-                    println!("Collision with wall - not moving");
                     return;
                 }
                 match self.rows.get(y) {
-                    Some(rock_points) => {
-                        if rock_points.contains(&new_x) {
-                            println!("Collision with rock - not moving");
+                    Some(r) => {
+                        if r.contains(&(max_x + 1)) {
                             return;
                         }
                     },
                     _ => ()
                 }
-                new_row.push(new_x);
-            };
+                new_row.retain(|x| { x != min_x});
+                new_row.push(max_x + 1);
+            } else {
+                new_row = shape_points.iter().map(|x| {x+1}).collect();
+                let new_max = new_row.iter().max().unwrap();
+                if new_max >= &SHAFT_WIDTH {
+                    return;
+                }
+                match self.rows.get(y) {
+                    Some(r) => {
+                        if r.contains(&new_max) {
+                            return;
+                        }
+                    },
+                    _ => ()
+                }
+            }
             new_rows.insert(y.to_owned(), new_row);
         }
         s.rows = new_rows;
@@ -156,6 +390,10 @@ impl Shaft {
          * ensure that all points can move right without:
          *  1) moving beyond the wall at SHAFT_WIDTH
          *  2) colliding with existing rock
+         * 
+         *  TODO: we only need to check for free space to the LEFT of the 
+         *      left-most point in each row, and then "move" the right-most
+         *      point into that free space
          */
         let mut new_rows = HashMap::new();
         for (y, shape_points) in s.rows.iter() {
@@ -163,13 +401,11 @@ impl Shaft {
             for x in shape_points.iter() {
                 let new_x = x - 1;
                 if new_x < 0 {
-                    println!("Collision with wall - not moving");
                     return;
                 }
                 match self.rows.get(y) {
                     Some(rock_points) => {
                         if rock_points.contains(&new_x) {
-                            println!("Collision with rock - not moving");
                             return;
                         }
                     },
@@ -187,13 +423,33 @@ impl Shaft {
 
         if jet_char == '>' {
             self.move_right();
-        } else {
+        } else if jet_char == '<' {
             self.move_left();
+        } else {
+            panic!("BAD CHAR {}", jet_char);
         }
+    }
+
+    fn prune_dead_rows(&mut self) {
+        let new_floor = if self.left_closure < self.right_closure {
+            self.left_closure
+        } else {
+            self.right_closure
+        };
+        
+        self.rows.retain( |k, _| { k >= &new_floor });
     }
 
     fn petrify_shape(&mut self) {
         let s = self.shape.as_mut().unwrap();
+        let mut new_floor = false;
+
+        // update high point
+        let shape_high_point = s.rows.keys().max().unwrap().to_owned();
+        if shape_high_point > self.high_point {
+            self.high_point = shape_high_point;
+        }
+
         for (y, shape_points) in s.rows.iter() {
             let mut row = match self.rows.remove(y) {
                 Some(r) => r,
@@ -202,10 +458,24 @@ impl Shaft {
             for x in shape_points.into_iter() {
                 row.push(x.clone());
             }
+            if row.contains(&0) {
+                self.left_closure = y.to_owned();
+                new_floor = true;
+
+            } else if row.contains(&(SHAFT_WIDTH - 1)) {
+                self.right_closure = y.to_owned();
+                new_floor = true;
+            }
             self.rows.insert(y.clone(), row);
         }
 
         self.shape = None;
+
+        /*
+        if new_floor {
+            self.prune_dead_rows();
+        }
+         */
     }
 
     fn gravity_turn(&mut self) -> bool {
@@ -228,7 +498,6 @@ impl Shaft {
                 match self.rows.get(&new_y) {
                     Some(rock_points) => {
                         if rock_points.contains(&x) {
-                            println!("Collision with rock - not falling");
                             self.petrify_shape();
                             return true;
                         }
@@ -246,7 +515,8 @@ impl Shaft {
 
     fn full_turn(&mut self) -> bool {
         self.jet_turn();
-        self.gravity_turn()
+        let rval = self.gravity_turn();
+        rval
     }
 
     fn drop_shape(&mut self) {
@@ -260,7 +530,7 @@ impl fmt::Display for Shaft {
 
         // TODO: account for when shape make highest point more than this
         let mut y = match self.shape {
-            None => self.high_point + 3,
+            None => self.high_point,
             Some(_) => self.shape.as_ref().unwrap().high_point()
         };
 
@@ -309,29 +579,49 @@ impl fmt::Display for Shaft {
     }
 }
 
-fn part_1(jets: JetStream) -> i32 {
+fn part_1(jets: JetStream) -> i64 {
     let mut shaft = Shaft::new(jets);
+    
+    for _ in 0..2022 {
+        shaft.add_next_shape();
+        shaft.drop_shape();
+    }
 
-    shaft.add_shape(Shape::horizontal_line(shaft.high_point));
-    println!("{}", shaft);
-    shaft.jet_turn();
-    println!("{}", shaft);
-    shaft.gravity_turn();
-    println!("{}", shaft);
-    
-    shaft.full_turn();
-    println!("{}", shaft);
-    
-    shaft.full_turn();
-    println!("{}", shaft);
-    
-    shaft.full_turn();
-    println!("{}", shaft);
+    if FILENAME == "./test" {
+        assert_eq!(shaft.high_point + 1, 3068);
+    }
 
-    shaft.high_point
+    shaft.high_point + 1
 }
 
-fn part_2() {
+fn part_2(jets: JetStream) -> i64 {
+    /*
+     * This is 1-trillion rocks, so it is not feasible to run the simultation
+     * for the 120 days it would take to calculate :-|
+     */
+    // 1,000,000,000,000;
+    let limit: i64 = 1000000000000;
+    let cycle_len = 1000000;
+
+    let mut shaft = Shaft::new(jets);
+
+    /*
+     * with 5 different shapes and
+     */
+    let mut prev_high = 0;
+    for i in 0..1 {
+        for _ in 0..cycle_len {
+            shaft.add_next_shape();
+            shaft.drop_shape();
+        }
+        println!("Cycle {}: {} (added {})",
+            i + 1,
+            shaft.high_point,
+            shaft.high_point - prev_high);
+        prev_high = shaft.high_point;
+    }
+
+    shaft.high_point + 1
 }
 
 fn main() {
@@ -343,14 +633,12 @@ fn main() {
 
     let now = Instant::now();
     use std::time::Instant;
-    part_1(jets);
+    println!("Part 1: {}", part_1(jets.clone()));
     let elapsed = now.elapsed();
     println!("Took {:.5?}", elapsed);
 
-    if false {
-        let now = Instant::now();
-        part_2();
-        let elapsed = now.elapsed();
-        println!("Took {:.5?}", elapsed);
-    }
+    let now = Instant::now();
+    println!("Part 2: {}", part_2(jets));
+    let elapsed = now.elapsed();
+    println!("Took {:.5?}", elapsed);
 }
