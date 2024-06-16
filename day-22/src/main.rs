5c58adbdc95a72cfad47a5a732810af5f400b383
bravochar <4663128+bravochar@users.noreply.github.com>
2024-06-16 12:56:55 -0400

    Day 22 Part 2 complete

--- day-22/src/main.rs
+++ day-22/src/main.rs
@@ -3,6 +3,7 @@ const FILENAME: &str = "./input";
 
 use std::fs::File;
 use std::io::{prelude::*, BufReader};
+use std::collections::HashMap;
 
 #[derive(Clone,Debug,PartialEq)]
 enum Tile {
@@ -46,12 +47,32 @@ enum Path {
     TurnLeft
 }
 
-#[derive(Clone,Debug)]
+#[derive(Clone,Copy,Debug,PartialEq,Eq,Hash)]
 enum Direction {
     Right,
     Down,
     Left,
     Up,
+}
+
+impl Direction {
+    fn turn_right(&self) -> Self {
+        match self {
+            Direction::Right => Direction::Down,
+            Direction::Down => Direction::Left,
+            Direction::Left => Direction::Up,
+            Direction::Up => Direction::Right
+        }
+    }
+
+    fn turn_left(&self) -> Self {
+        match self {
+            Direction::Left => Direction::Down,
+            Direction::Up => Direction::Left,
+            Direction::Right => Direction::Up,
+            Direction::Down => Direction::Right
+        }
+    }
 }
 
 fn read_file(filename: &str) -> (Vec<Vec<Tile>>, Vec<Path>) {
@@ -201,7 +222,7 @@ fn move_on_map(
     let mut new_x;
     let mut new_y;
 
-    println!("Moving {} {:?}", dist, dir);
+    //println!("Moving {} {:?}", dist, dir);
 
     for _ in 0..*dist {
         new_x = x;
@@ -230,7 +251,7 @@ fn move_on_map(
             Direction::Down => {
                 new_x += 1;
                 if new_x >= map.len() {
-                    println!("Moved off of map! {} -> {}", x, new_x);
+                    //println!("Moved off of map! {} -> {}", x, new_x);
                     new_x = find_first_x(&new_y, map);
                 } else {
                     new_x = match map.get(new_x).unwrap().get(new_y) {
@@ -271,10 +292,224 @@ fn move_on_map(
 
         x = new_x;
         y = new_y;
-        println!("Moved to {}, {}", x, y);
+        //println!("Moved to {}, {}", x, y);
     }
 
     (x, y)
+}
+
+fn valid_move(p: (usize, usize), d: Direction, m: usize, map: &Vec<Vec<Tile>>) -> Option<(usize, usize)> {
+    let (mut x, mut y) = p;
+
+    // get new coords, checking for map bounds
+    match d {
+        Direction::Down => {
+            y += m;
+        },
+        Direction::Up => {
+            if m > y {
+                return None;
+            } else {
+                y -= m;
+            }
+        }
+        Direction::Left => {
+            if m > x {
+                return None;
+            } else {
+                x -= m;
+            }
+        },
+        Direction::Right => {
+            x += m;
+        }
+    }
+
+    // verify new coord is not Tile::None
+    match map.get(y) {
+        Some(r) => {
+            match r.get(x) {
+                Some(t) => {
+                    match t {
+                        Tile::Nope => return None,
+                        _ => return Some((x, y))
+                    };
+                },
+                _ => return None,
+            };
+        },
+        _ => return None
+    };
+}
+
+fn zip_edges_pair(
+    e1: &((usize, usize), (usize, usize), Direction),
+    e2: &((usize, usize), (usize, usize), Direction),
+    map: &Vec<Vec<Tile>>
+        ) -> HashMap<((usize, usize), Direction), ((usize, usize), Direction)> {
+    let mut rval = HashMap::new();
+
+    let mut p1 = e1.1;
+    let mut p2 = e2.0;
+    let e1_dir = e1.2.turn_left();
+    let e2_dir = e2.2.turn_right();
+    let p1_dir = e1_dir.turn_left();
+    let p2_dir = e2_dir.turn_right();
+
+    loop {
+        //println!("    ({}, {}) {:?} -> ({}, {}) {:?}",
+        //    p1.0, p1.1, e1.2, p2.0, p2.1, p2_dir);
+        rval.insert(((p1.0, p1.1), e1.2), ((p2.0, p2.1), p2_dir));
+        //println!("    ({}, {}) {:?} -> ({}, {}) {:?}",
+        //    p2.0, p2.1, e2.2, p1.0, p1.1, p1_dir);
+        rval.insert(((p2.0, p2.1), e2.2), ((p1.0, p1.1), p1_dir));
+
+        // check for end condition
+        if p1 == e1.0 {
+            break;
+        }
+
+        // increment points
+        p1 = valid_move(p1, e1_dir, 1, map).unwrap();
+        p2 = valid_move(p2, e2_dir, 1, map).unwrap();
+    }
+    
+    rval
+}
+
+fn zip_edges(
+            mut i: usize,
+            edges: &Vec<((usize, usize), (usize, usize), Direction)>,
+            map: &Vec<Vec<Tile>>
+        ) -> HashMap<((usize, usize), Direction), ((usize, usize), Direction)> {
+    let mut rval = HashMap::new();
+
+    //println!("Zipping form inside corner");
+
+    let mut j = (i + 1) % edges.len();
+    let mut edge_1 = edges.get(i).unwrap();
+    let mut edge_2 = edges.get(j).unwrap();
+
+
+    loop {
+        // zip together edges
+        //println!("  Zip edges: {:?} {:?}", edge_1, edge_2);
+        rval.extend(zip_edges_pair(edge_1, edge_2, map));
+
+        // get next edges
+        i = if i > 0 {
+            i - 1
+        } else {
+            edges.len() - 1
+        };
+        j = (j + 1) % edges.len();
+
+        let new_1 = edges.get(i).unwrap();
+        let new_2 = edges.get(j).unwrap();
+
+        if new_1.2 != edge_1.2 && new_2.2 != edge_2.2 {
+            // both next edges were around corners - we're done
+            break;
+        }
+
+        edge_1 = new_1;
+        edge_2 = new_2;
+    }
+
+    rval
+}
+
+fn build_wrap_map(map: &Vec<Vec<Tile>>
+        ) -> HashMap<((usize, usize), Direction), ((usize, usize), Direction)> {
+    let mut rval = HashMap::new();
+    let mut x: usize = 0;
+    let mut edge_dir = Direction::Up;
+    let mut trace_dir = Direction::Right;
+
+    // find starting x
+    for (i, t) in map.get(0).unwrap().iter().enumerate() {
+        match t {
+            Tile::Open => {
+                x = i;
+                break;
+            },
+            _ => ()
+        };
+    }
+
+    // divine length of side of cube
+    let map_height = map.len();
+    let mut map_widths = Vec::new();
+    for row in map.iter() {
+        if !map_widths.contains(&row.len()) {
+            map_widths.push(row.len());
+        }
+    }
+    let map_width = map_widths.iter().max().unwrap().clone();
+    let side_length = if map_width / 3 * 4 == map_height {
+        map_width / 3
+    } else if map_height / 3 * 4 == map_width {
+        map_height / 3
+    } else {
+        panic!("MATH HAS FAILED US");
+    };
+    let edge_add = side_length - 1;
+
+    // find all of the edges and the direction "off" that edge
+    let mut edges = Vec::new();
+    let start = (x, 0);
+    let mut cur = start.clone();
+    let mut edge_end = (cur.0 + edge_add, cur.1);
+    edges.push( (cur, edge_end, edge_dir) );
+    loop {
+        // find start and direction of the next edge
+        match valid_move(edge_end, trace_dir, 1, map) {
+            None => {
+                // outside corner
+                cur = edge_end;
+                edge_dir = edge_dir.turn_right();
+                trace_dir = trace_dir.turn_right();            },
+            Some(p) => {
+                match valid_move(p, edge_dir, 1, map) {
+                    None => {
+                        // parallel edge
+                        cur = p;
+                    },
+                    Some(p) => {
+                        // inside corner
+                        cur = p;
+                        edge_dir = edge_dir.turn_left();
+                        trace_dir = trace_dir.turn_left();
+                    }
+                }
+
+            }
+        };
+        edge_end = valid_move(cur, trace_dir, edge_add, map).unwrap();
+        
+        if cur == start {
+            break;
+        } else {
+            edges.push( (cur, edge_end, edge_dir) );
+        }
+    }
+
+    for edge in edges.iter() {
+        //println!("Edge from {:?} to {:?} pointed {:?}",
+        //    edge.0, edge.1, edge.2);
+    }
+
+    // find an inside corner and "zip" it up
+    for (i, e_t) in edges.windows(2).enumerate() {
+        let edge_1 = e_t[0];
+        let edge_2 = e_t[1];
+
+        if edge_1.2.turn_left() == edge_2.2 {
+            rval.extend(zip_edges(i, &edges, map));
+        }
+    }
+
+    rval
 }
 
 fn part_1(map: Vec<Vec<Tile>>, password: Vec<Path>) -> i64 {
@@ -282,6 +517,7 @@ fn part_1(map: Vec<Vec<Tile>>, password: Vec<Path>) -> i64 {
     let mut x = 0;
     let mut y = 0;
 
+    // find starting y
     for (i, t) in map.get(0).unwrap().iter().enumerate() {
         match t {
             Tile::Open => {
@@ -297,20 +533,10 @@ fn part_1(map: Vec<Vec<Tile>>, password: Vec<Path>) -> i64 {
     for m in password.iter() {
         match m {
             Path::TurnRight => {
-                dir = match dir {
-                    Direction::Right => Direction::Down,
-                    Direction::Down => Direction::Left,
-                    Direction::Left => Direction::Up,
-                    Direction::Up => Direction::Right,
-                };
+                dir = dir.turn_right();
             },
             Path::TurnLeft => {
-                dir = match dir {
-                    Direction::Left => Direction::Down,
-                    Direction::Up => Direction::Left,
-                    Direction::Right => Direction::Up,
-                    Direction::Down => Direction::Right,
-                };
+                dir = dir.turn_left();
             },
             Path::Move(dist) => {
                 (x, y) = move_on_map(x, y, dist, &dir, &map);
@@ -326,8 +552,108 @@ fn part_1(map: Vec<Vec<Tile>>, password: Vec<Path>) -> i64 {
     }
 }
 
-fn part_2(_map: Vec<Vec<Tile>>) -> i64 {
-    0
+fn move_around_cube(
+        mut x: usize,
+        mut y: usize,
+        dist: &i32,
+        mut dir: Direction,
+        map: &Vec<Vec<Tile>>,
+        wrap: &HashMap<((usize, usize), Direction), ((usize, usize), Direction)>
+    ) -> (usize, usize, Direction) {
+    let mut new_x;
+    let mut new_y;
+    let mut new_dir = dir.clone();
+
+    //println!("Moving {} {:?}", dist, dir);
+
+    for _ in 0..*dist {
+        new_x = x;
+        new_y = y;
+
+        if wrap.contains_key(&((x, y), dir.clone())) {
+            //println!("Getting wrap point for {}, {}", x, y);
+            let p = wrap.get(&((x, y), dir.clone())).unwrap();
+            new_x = p.0.0.clone();
+            new_y = p.0.1.clone();
+            new_dir = p.1.clone();
+        } else {
+            match dir {
+                Direction::Up => { new_y -= 1; },
+                Direction::Right => { new_x += 1; },
+                Direction::Down => { new_y += 1; },
+                Direction::Left => { new_x -= 1; },
+            }
+        }
+
+        // check for valid move
+        let row = map.get(new_y).unwrap();
+        match row.get(new_x) {
+            Some(Tile::Wall) => {
+                // done moving if we hit a wall, return previous coords
+                //println!("Hit wall at {}, {}", new_x, new_y);
+                break;
+            },
+            Some(Tile::Open) => (),
+            Some(Tile::Nope) => {
+                println!("NOPE At {}, {}", new_x, new_y);
+                panic!("Off of map: {:?}", row);
+            },
+            None => {
+                println!("Out of bounds at {}, {}", new_x, new_y);
+                panic!("Out of bounds: {:?}", row);
+            }
+        }
+
+        x = new_x;
+        y = new_y;
+        dir = new_dir;
+        //println!("Moved to ({}, {}) {:?}", x, y, dir);
+    }
+
+    (x, y, dir)
+}
+
+fn part_2(map: Vec<Vec<Tile>>, password: Vec<Path>) -> i64 {
+    let wrap = build_wrap_map(&map);
+    let mut dir = Direction::Right;
+    let mut x = 0;
+    let mut y = 0;
+
+    //println!("{:?}", wrap);
+
+    // find starting y
+    for (i, t) in map.get(0).unwrap().iter().enumerate() {
+        match t {
+            Tile::Open => {
+                x = i;
+                break;
+            },
+            _ => ()
+        };
+    }
+    
+    println!("Starting at {}, {}", x, y);
+
+    for m in password.iter() {
+        match m {
+            Path::TurnRight => {
+                dir = dir.turn_right();
+            },
+            Path::TurnLeft => {
+                dir = dir.turn_left();
+            },
+            Path::Move(dist) => {
+                (x, y, dir) = move_around_cube(x, y, dist, dir, &map, &wrap);
+            }
+        };
+    }
+
+    1000 * (y as i64 + 1) + 4 * (x as i64 + 1) + match dir {
+        Direction::Right => 0,
+        Direction::Down => 1,
+        Direction::Left => 2,
+        Direction::Up => 3
+    }
 }
 
 fn main() {
@@ -336,8 +662,8 @@ fn main() {
     // read in the input
     let (input, password) = read_file(FILENAME);
 
-    println!("{}", map_to_string(&input));
-    println!("{:?}", password);
+    //println!("{}", map_to_string(&input));
+    //println!("{:?}", password);
 
     let now = Instant::now();
     use std::time::Instant;
@@ -353,7 +679,7 @@ fn main() {
     }
 
     let now = Instant::now();
-    let answer = part_2(input.clone());
+    let answer = part_2(input.clone(), password.clone());
     let elapsed = now.elapsed();
     println!("Part 2: {}", answer);
     println!("Took {:.5?}", elapsed);
@@ -361,6 +687,6 @@ fn main() {
     if FILENAME == "./test" {
         assert_eq!(answer, 5031);
     } else {
-        //assert_eq!(answer, 3769668716709);
+        assert_eq!(answer, 197122);
     }
 }
